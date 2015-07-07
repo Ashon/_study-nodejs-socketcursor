59ae4197ec1c502dadf63c55c6adb34641b90e0a
ashon <ashon8813@gmail.com>
2015-07-07 23:23:12 +0900

    update

--- cursor.js
+++ cursor.js
@@ -1,16 +1,16 @@
 var cursor = module.exports = {
-	users : [],
-	hasUser : function(name){
-		var users = this.users.filter(function(element){
-			return element === name;
-		});
+    users : [],
+    hasUser : function(name){
+        var users = this.users.filter(function(element){
+            return element === name;
+        });
 
-		if(users.length > 0)
-			return true;
-		else
-			return false;
-	},
-	addUser : function(name){
-		this.users.push(name);
-	}
+        if(users.length > 0)
+            return true;
+        else
+            return false;
+    },
+    addUser : function(name){
+        this.users.push(name);
+    }
 };
